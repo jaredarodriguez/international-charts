004a76703c7a3c653abc14bb2f24ba29556d82c9
Jared <jaredarodriguez@gmail.com>
2020-08-16 20:39:21 -0400

    track cards display track info

--- script.js
+++ script.js
@@ -1,35 +1,41 @@
-const artist_name = document.getElementById('artist_name');
-const track_name = document.getElementById('track_name');
-const album_name = document.getElementById('album_name')
+const cardContainer = document.getElementById("card-container");
 
-
-
-import config from './env.js'
+import config from "./env.js";
 // Get lyric from API
-    async function getPopularMusic() {
-        const proxyURL = 'https://cors-anywhere.herokuapp.com/';
-        const apiURL = `https://api.musixmatch.com/ws/1.1/chart.tracks.get?chart_name=top&page=1&page_size=5&country=it&f_has_lyrics=1&apikey=${config.apiKey}`
-        try {
-            const response = await fetch(proxyURL + apiURL);
-            const data = await response.json();
-            const trackList = data.message.body.track_list;
-            
-            trackList.map((e) => {
-                // Render Artist Name
-                artist_name.innerText = e.track.artist_name;
-                // Render Track Name
-                track_name.innerText = e.track.track_name;
-                // Render Album Name 
-                album_name.innerText = e.track.album_name;
-            })
-        } catch (error) {
-            console.log(error);
-        }
-    }
+async function displayPopularMusic() {
+  const proxyURL = "https://cors-anywhere.herokuapp.com/";
+  const apiURL = `https://api.musixmatch.com/ws/1.1/chart.tracks.get?chart_name=top&page=1&page_size=5&country=it&f_has_lyrics=1&apikey=${config.apiKey}`;
 
-    //on load 
-    getPopularMusic(); 
+  try {
+    const response = await fetch(proxyURL + apiURL);
+    const data = await response.json();
+    const trackList = data.message.body.track_list;
+
+    trackList.forEach(function (track) {
+    // Create wrapper for track data
+      const wrapperDiv = document.createElement("div");
+      wrapperDiv.id = 'wrapper-div'
+    // Create Artist, track and album and add ID p
+      var artistDiv = document.createElement("h1")
+      var trackDiv = document.createElement("div");
+      var albumDiv = document.createElement("div");
+      artistDiv.append(document.createTextNode(track.track.artist_name));
+      trackDiv.append(document.createTextNode(track.track.track_name));
+      albumDiv.append(document.createTextNode(track.track.album_name));
+    // Append Artist, track and album data to wrapper
+      wrapperDiv.append(artistDiv);
+      wrapperDiv.append(trackDiv);
+      wrapperDiv.append(albumDiv);
+    // Style wrapper 
+    // Append wrapper to container div
+      cardContainer.appendChild(wrapperDiv);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
 
 
-    
 
+//on load
+displayPopularMusic();
