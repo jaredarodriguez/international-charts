24284fc1452bc5b9f9ea2f982a3b881690b802fa
Jared <jaredarodriguez@gmail.com>
2020-08-18 21:32:36 -0400

    add side menu

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-const countryContainer = document.getElementById("country-container");
-
 import config from "./env.js";
-// Get lyric from API
+
+// DOM elements
+const countryContainer = document.getElementById("country-container");
+const openSlide = document.getElementById('open-slide');
+const closeSlide = document.getElementById('close-slide');
+
+// Get top hits from API
 async function displayPopularMusic() {
   const proxyURL = "https://cors-anywhere.herokuapp.com/";
   const apiURL = `https://api.musixmatch.com/ws/1.1/chart.tracks.get?chart_name=top&page=1&page_size=5&country=it&f_has_lyrics=1&apikey=${config.apiKey}`;
@@ -21,9 +25,9 @@ async function displayPopularMusic() {
       var albumDiv = document.createElement("div");
     // Reduce font size depending on artist_name length
     track.track.artist_name.length > 20 ? artistDiv.classList.add('long-title') : artistDiv.classList.remove('long-title')
-      artistDiv.append(document.createTextNode(track.track.artist_name));
-      trackDiv.append(document.createTextNode(track.track.track_name));
-      albumDiv.append(document.createTextNode(track.track.album_name));
+      artistDiv.append(document.createTextNode(`Artist: ${track.track.artist_name}`));
+      trackDiv.append(document.createTextNode(`Track: ${track.track.track_name}`));
+      albumDiv.append(document.createTextNode(`Album: ${track.track.album_name}`));
     // Append Artist, track and album data to wrapper
       wrapperDiv.append(artistDiv);
       wrapperDiv.append(trackDiv);
@@ -36,8 +40,20 @@ async function displayPopularMusic() {
     console.log(error);
   }
 }
-
-
-
-//on load
 displayPopularMusic();
+
+
+// Events 
+function openSlideMenu() {
+  console.log('hit')
+  document.getElementById('side-menu').style.width = '250px';
+  document.getElementById('main').style.marginLeft = '250px';
+}
+
+function closeSlideMenu() {
+  sideMenu.style.width = '0'
+}
+ 
+// Event Listeners
+openSlide.addEventListener('click', openSlideMenu);
+closeSlide.addEventListener('click', closeSlideMenu);
